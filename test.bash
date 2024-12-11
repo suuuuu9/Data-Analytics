334b7a4bc4cc50d878c2f7e89e2e17309bdab933
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2024-12-11 14:15:34 +0900

    引数対応

--- test.bash
+++ test.bash
@@ -12,28 +12,13 @@ res=0
 ### 正常な入力 ###
 #正の値
 out=$(seq 5 | ./analytics)
-output="平均値 3.00
-最大値 5
-最小値 1
-分散 2.00
-標準偏差 1.41"
-[ "${out}" = "${output}" ] || ng "$LINENO"
+[ "${out}" = 3.00 5 1 2.00 1.41 ] || ng "$LINENO"
 #負の値
 out=$(echo -1 -2 -3 -4 -5 | tr ' ' '\n' | ./analytics)
-output="平均値 -3.00
-最大値 -1
-最小値 -5
-分散 2.00
-標準偏差 1.41"
-[ "${out}" = "${output}" ] || ng "$LINENO"
+[ "${out}" = -3.00 -1 -5  2.00 1.41 ] || ng "$LINENO"
 #小数点
 out=$(echo 1.1 2.2 3.3 4.4 5.5 | tr ' ' '\n' | ./analytics)
-output="平均値 3.30
-最大値 5.5
-最小値 1.1
-分散 2.42
-標準偏差 1.56"
-[ "${out}" = "${output}" ] || ng "$LINENO"
+[ "${out}" = 3.30 5.5 1.1 2.42 1.56 ] || ng "$LINENO"
 
 ### 変な入力 ###
 out=$(echo あ| ./analytics)
